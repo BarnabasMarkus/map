3848d0e4a71049e84b1a739af4fb3d94db71c2f0
barnabas markus <barnabasmarkus@gmail.com>
2018-03-19 20:17:09 +0100

    update and insert functions added

--- map.hs
+++ map.hs
@@ -10,20 +10,24 @@ find key (Map ((k, v):xs)) = if key == k
                              then Just v
                              else find key (Map xs)
 
--- insert :: Eq a => a -> b -> Map a b
--- insert key value (Map ((k, v): xs)) = undefined
+insert :: Eq a => a -> b -> Map a b -> Map a b
+insert key value map@(Map xs) = 
+  fromList $ (key, value) : xs'
+  where xs' = if isElem key map
+              then filter (\(k, v) -> k /= key) xs
+              else xs
 
-update :: Eq a => a -> b -> Map a b
-update = undefined 
+update :: Eq a => a -> b -> Map a b -> Map a b
+update = insert
 
 fromList :: Eq a => [(a, b)] -> Map a b
 fromList xs = Map xs
 
---isElem :: Eq a => a -> Map a b -> Bool
--- isElem key (Map xs) = elem . keys $ (Map xs)
-
 toList :: Map a b -> [(a, b)]
 toList (Map xs) = xs
+
+isElem :: Eq a => a -> Map a b -> Bool
+isElem key (Map xs) = elem key $ keys (Map xs)
 
 isNull :: Map a b -> Bool
 isNull (Map xs) = null xs
@@ -49,14 +53,20 @@ info = ["Simple map implementation in haskell"
        ,"* find"
        ,"  find 2 myLittleMap"
        ,""
-       ,"* update"
-       ,"  undefined"
+       ,"* insert"
+       ,"  insert 2 'X' myLittleMap"
+       ,""
+       ,"* update (same as insert)"
+       ,"  update 2 'X' myLittleMap"
        ,""
        ,"* fromList"
        ,"  let myLittleMap = fromList [(1, 'a'), (2, 'b'), (3, 'c')]"
        ,""
        ,"* toList"
        ,"  toList myLittleMap"
+       ,""
+       ,"* isElem"
+       ,"  isElem 2 myLittleMap"
        ,""
        ,"* isNull"
        ,"  isNull myLittleMap"
