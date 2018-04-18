9fee1f4a4f00722fb87ea0ac8031233d1696aee7
barnabas markus <barnabasmarkus@gmail.com>
2018-04-18 20:49:17 +0200

    instance semigroup, monoid

--- map.hs
+++ map.hs
@@ -1,8 +1,47 @@
+{- M A P   D A T A   S T R U C T U R E -}
 
-module Map where
+
+module Map 
+  ( Map (..)
+  , find
+  , insert
+  , update
+  , fromList
+  , toList
+  , isElem
+  , isNull
+  , len
+  , keys
+  , vals
+  , sortByKeys
+  , pretty
+  -- , fromFile
+  -- , toFile
+  )
+  where
+
+
+import Data.Semigroup
 
 
 data Map k v = Map [(k, v)] deriving (Show, Eq)
+
+
+-- Be careful: keys have to stay unique!!!
+--instance Functor (Map a) where
+--  fmap _ (Map []) = Map []
+--  fmap f (Map xs) = Map xs'
+--    where xs' = map (\(k, v) -> (k, v)) xs
+
+
+instance (Eq a) => Semigroup (Map a b) where
+  (<>) (Map xs) (Map ys) = fromList $ xs ++ ys
+
+
+instance (Eq a) => Monoid (Map a b) where
+  mempty = Map []
+  mappend = (<>)
+
 
 find :: Eq a => a -> Map a b -> Maybe b
 find _ (Map []) = Nothing
@@ -11,9 +50,9 @@ find key (Map ((k, v):xs)) = if key == k
                              else find key (Map xs)
 
 insert :: Eq a => a -> b -> Map a b -> Map a b
-insert key value map@(Map xs) = 
-  fromList $ (key, value) : xs'
-  where xs' = if isElem key map
+insert key value m@(Map xs) = 
+  Map $ (key, value) : xs'
+  where xs' = if isElem key m
               then filter (\(k, v) -> k /= key) xs
               else xs
 
@@ -21,7 +60,7 @@ update :: Eq a => a -> b -> Map a b -> Map a b
 update = insert
 
 fromList :: Eq a => [(a, b)] -> Map a b
-fromList xs = Map xs
+fromList xs = foldr (\(k,v) -> insert k v) (Map []) xs
 
 toList :: Map a b -> [(a, b)]
 toList (Map xs) = xs
@@ -52,66 +91,10 @@ sortByKeys m@(Map xs) =
   let keys = sort $ map fst xs
   in fromList $ [(k, snd x) | k <- keys, x <- xs, k == fst x]
 
-{--
-TODO: works only with unique values, remove duplicates
-sortByVals :: (Eq a, Ord b) => Map a b -> Map a b
-sortByVals m@(Map xs) = 
-  let vals = sort $ map snd xs
-  in fromList $ [ (fst x, v) | v <- vals, x <- xs, v == snd x]
---}
-
 pretty :: (Show a, Show b) => Map a b -> IO ()
 pretty (Map xs) = 
   mapM_ (putStrLn . (\(k,v) -> show k ++ " : " ++ show v )) xs
 
-info :: [String]
-info = ["Simple map implementation in haskell"
-       ,""
-       ,"Functions and examples:"
-       ,""
-       ,"* find"
-       ,"  find 2 myLittleMap"
-       ,""
-       ,"* insert"
-       ,"  insert 2 'X' myLittleMap"
-       ,""
-       ,"* update (same as insert)"
-       ,"  update 2 'X' myLittleMap"
-       ,""
-       ,"* fromList"
-       ,"  let myLittleMap = fromList [(1, 'a'), (2, 'b'), (3, 'c')]"
-       ,""
-       ,"* toList"
-       ,"  toList myLittleMap"
-       ,""
-       ,"* isElem"
-       ,"  isElem 2 myLittleMap"
-       ,""
-       ,"* isNull"
-       ,"  isNull myLittleMap"
-       ,""
-       ,"* len"
-       ,"  len myLittleMap"
-       ,""
-       ,"* keys"
-       ,"  keys myLittleMap"
-       ,""
-       ,"* vals"
-       ,"  vals myLittleMap"
-       ,""
-       ,"* sortByKeys"
-       ,"  sortByKeys myLittleMap"
-       ,""
-       ,"* pretty"
-       ,"  pretty myLittleMap"
-       ,""
-       ,"* getInfo"
-       ,""
-       ,"...etc"
-       ,""
-       ]
+fromFile = undefined
 
-getInfo :: IO ()
-getInfo = mapM_ putStrLn info
-
-
+toFile = undefined
